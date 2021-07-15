f5956420bc1efd23de03e73312426b9db43f5182
김지환 <bure5kzam@gmail.com>
2021-07-15 09:17:29 +0900

    create conf.js

--- AWS/front/fhth/src/main.js
+++ AWS/front/fhth/src/main.js
@@ -1,25 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import "./assets/css/all.css"
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css';
-
-Vue.config.productionTip = false
-Vue.use(ElementUI)
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./assets/css/all.css";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import { baseURL, port } from "./utils/conf";
+Vue.config.productionTip = false;
+Vue.use(ElementUI);
 
 // vue-socket.io 설정
-import io from "socket.io-client"
-const socket = io("http://54.180.202.172:8080", {
-withCredentials: true,
-extraHeaders: {
-      "my-custom-header": "abcd"
-}})
+import io from "socket.io-client";
 
-Vue.prototype.$socket = socket
+const socket = io(baseURL + port.server, {
+  withCredentials: true,
+  extraHeaders: {
+    "my-custom-header": "abcd",
+  },
+});
+Vue.prototype.$socket = socket;
+
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
--- AWS/front/fhth/vue.config.js
+++ AWS/front/fhth/vue.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  devServer: {
-    proxy: 'http://localhost:8081',
-  },
  publicPath: "/FHTH_free/",
   outputDir: "dist"
 }
