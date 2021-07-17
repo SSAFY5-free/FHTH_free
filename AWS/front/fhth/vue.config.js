2e48aa95dbdb7b1e1e7594eb22b7092ab46946ae
Jangbeomjin <qjawls0501@naver.com>
2021-07-17 16:01:11 +0900

    merge com

--- AWS/front/fhth/vue.config.js
+++ AWS/front/fhth/vue.config.js
@@ -1,10 +1,7 @@
 module.exports = {
-<<<<<<< HEAD
   devServer: {
     proxy: 'http://54.180.202.172:8080'
-  }
-=======
+  },
  publicPath: "/FHTH_free/",
   outputDir: "dist"
->>>>>>> 6be2739f6086d652afbd5461ae99e565a5c567ce
 }
