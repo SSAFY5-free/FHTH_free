ab3c508834a6d108df32921dba3fc60998b457db
bure5kzam <bure5kzam@gmail.com>
2021-09-19 22:34:26 +0900

    fix : reservation 자료형 배열 => 날짜

--- AWS/backend/seeders/20210825122739-registed-modules.js
+++ AWS/backend/seeders/20210825122739-registed-modules.js
@@ -17,14 +17,15 @@ module.exports = {
         serial: "970121",
         type_id: 1,
         data: { isEaten: false, timeEaten: moment().format(), numEaten: 30, timeWater: null, isWater: null },
-        reservation: [moment().add(1, "day"), moment().add(1, "minute")],
+        // reservation: [moment().add(1, "day"), moment().add(1, "minute")],
+        reservation: moment(),
         name: "module1"
       })
     await models["registedModules"].create({
       serial: "970232",
       type_id: 2,
       data: { isEaten: null, timeEaten: null, numEaten: null, timeWater: moment().format(), isWater: true },
-      reservation: [moment().add(1, "day"), moment().add(1, "minute")],
+      // reservation: [moment().add(1, "day"), moment().add(1, "minute")],
       name: "module2"
     })
     return await models["registedModules"].create(
@@ -32,7 +33,8 @@ module.exports = {
         serial: "970343",
         type_id: 1,
         data: { isEaten: true, timeEaten: moment().format(), numEaten: 90, timeWater: null, isWater: null },
-        reservation: [moment().add(1, "day"), moment().add(1, "minute")],
+        // reservation: [moment().add(1, "day"), moment().add(1, "minute")],
+        reservation: moment(),
         name: "module3"
       }
     ).then((res) => {
