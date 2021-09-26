c2d8b6477c0c8e08f7ad747ea53b16401dacda7b
ssej0221 <ssej0221@gmail.com>
2021-09-26 12:21:43 +0900

    fix: fix json data

--- backend_rpi/routes/pet.js
+++ backend_rpi/routes/pet.js
@@ -13,52 +13,52 @@ router.get("/", function (req, res, next) {
 });
 
 //set_amount 제어
-// router.post("/feedcontrol", async function (req, res, next) {
-//   bus.getInterface("food.fhth","/fhth/food/Test", "food.fhth.TestInterface", function(err, iface) {
-//     if(err){
-//       console.log(err)
-//     }
-//     iface.set_amount(req.body.payload)
-//     console.log("success")
-//     res.send(req.body)
-//   })
-// });
+router.post("/feedcontrol", async function (req, res, next) {
+  bus.getInterface("food.fhth","/fhth/food/Test", "food.fhth.TestInterface", function(err, iface) {
+    if(err){
+      console.log(err)
+    }
+    iface.set_amount(req.body.payload)
+    console.log("success")
+    res.send(req.body)
+  })
+});
 
 //robot 제어
-// router.post("/control", async function (req, res, next) {
-//   bus.getInterface("motor.fhth","/fhth/motor/Test", "motor.fhth.TestInterface", function(err, iface) {
-//     if(err){
-//       console.log(err)
-//     }
-//     iface.activate_motor(req.body.payload)
-//     console.log("success")
-//     res.send(req.body)
-//   })
-// });
+router.post("/control", async function (req, res, next) {
+  bus.getInterface("motor.fhth","/fhth/motor/Test", "motor.fhth.TestInterface", function(err, iface) {
+    if(err){
+      console.log(err)
+    }
+    iface.activate_motor(req.body.payload)
+    console.log("success")
+    res.send(req.body)
+  })
+});
 
 
-// 밥주기 예약 및 밥 주기
-// router.post("/servefood", async function (req, res, next) {
-//   bus.getInterface("food.fhth","/fhth/food/Test", "food.fhth.TestInterface", function(err, iface) {
-//     if(err){
-//       console.log(err)
-//     }
-//     iface.activate_action()
-//     console.log("success")
-//     res.send("success")
-//   })
-// });
+//밥주기 예약 및 밥 주기
+router.post("/servefood", async function (req, res, next) {
+  bus.getInterface("food.fhth","/fhth/food/Test", "food.fhth.TestInterface", function(err, iface) {
+    if(err){
+      console.log(err)
+    }
+    iface.activate_action()
+    console.log("success")
+    res.send("success")
+  })
+});
 
 // 밥 먹은거  타임 스탬프 추가해야함
 router.post("/foodeat", async function (req, res, next) {
-  iseaten = req.body.data.EATEN
+  iseaten = req.body.EATEN
   await axios
     .post("http://127.0.0.1:8079/unauth/setModule", {
       module_id: 2,
       data: {
         iseaten: iseaten,
         left: left,
-        time: req.body.data.DATE
+        time: req.body.DATE
       },
     })
     .then((response) => {
@@ -75,7 +75,7 @@ router.post("/foodeat", async function (req, res, next) {
 });
 // 밥 남은거
 router.post("/foodleft", async function (req, res, next) {
-  left = req.body.data.LEFT
+  left = req.body.LEFT
   res.json(req.body);
   await axios
     .post("http://127.0.0.1:8079/unauth/setModule", {
@@ -83,7 +83,7 @@ router.post("/foodleft", async function (req, res, next) {
       data: {
         iseaten: iseaten,
         left: left,
-        time: req.body.data.DATE
+        time: req.body.DATE
       },
     })
     .then((response) => {
@@ -101,15 +101,17 @@ router.post("/foodleft", async function (req, res, next) {
 
 // 물 먹었는지
 router.post("/waterdrink", async function (req, res, next) {
-  drink = req.body.data.DRINK
+  console.log(req.body)
+  drink = req.body.DRINK
   res.json(req.body);
+
   await axios
     .post("http://127.0.0.1:8079/unauth/setModule", {
       module_id: 1,
       data: {
         drink: drink,
         water: water,
-        time: req.body.data.DATE
+        time: req.body.DATE
       },
     })
     .then((response) => {
@@ -127,7 +129,7 @@ router.post("/waterdrink", async function (req, res, next) {
 
 // 물 부족한지
 router.post("/waterlack", async function (req, res, next) {
-  water = req.body.data.WATER_LACK
+  water = req.body.WATER_LACK
   res.json(req.body);
   await axios
     .post("http://127.0.0.1:8079/unauth/setModule", {
@@ -135,7 +137,7 @@ router.post("/waterlack", async function (req, res, next) {
       data: {
         drink: drink,
         water: water,
-        time: req.body.data.DATE
+        time: req.body.DATE
       },
     })
     .then((response) => {
